bf9e96c1d7f6a31c0381ddef4b09fa0af7eec7a7
gogoend <gogoend@qq.com>
2020-02-09 17:12:51 +0800

    enhance: add the pause and resume function.

--- src/animation.js
+++ src/animation.js
@@ -1,104 +1,134 @@
 import { easingFuncs as ease } from './easing_funcs.js';
-import { clamp,getStyle } from './util.js';
+import { clamp, getStyle } from './util.js';
 
-class Animation{
-    constructor(el,queue,duration = 2000, easeType = 'quadraticInOut', manualNext = false ){
-        this.el=el;
-        this.queue=queue;
-        this.duration=duration;
-        this.easeType=easeType;
-        this.manualNext=manualNext
+class Animation {
 
-        this.i=0;
+    constructor(el, queue, duration = 2000, easeType = 'quadraticInOut', manualNext = false) {
+        this.el = el;
+        this.queue = queue;
+        this.duration = duration;
+        this.easeType = easeType;
+        this.manualNext = manualNext;
 
-        if(!queue[0]){
+        this.i = 0;
+
+        this.status = {
+            paused: false
+        };
+
+        this.status.paused = false
+
+        if (!queue[0]) {
             return
-        }else{
-            this.animationQueueHandler=this.go();
-            setTimeout(()=>this.animationQueueHandler.next(),queue[0].delay)
+        }
+        if (!this.manualNext) {
+            this.animationQueueHandler = this.go();
+            setTimeout(() => this.animationQueueHandler.next(), this.queue[0].delay)
         }
 
         // debugger
 
     }
-    *go(){
-        let {i,queue,executor}=this;
+    pause() {
+        let { startTime } = this.status;
+        let duration = this.queue[this.i + 1].duration;
+        this.status.paused = true;
+        let pausedTime = new Date().getTime();
+        this.status.passedTime = pausedTime - startTime;
+    }
+    resume() {
+        let { status }=this;
+
+        status.startTime=new Date().getTime()-status.passedTime
+        this.status.paused = false;
+
+    }
+    *go() {
+        let { i, queue, executor } = this;
         while (i <= queue.length) {
             yield queue[i + 1] ? (
                 executor(
-                    // el, 
-                    // queue[i].props, 
-                    // queue[++i].props, 
-                    // queue[i].duration?queue[i].duration:duration, 
-                    // ease[queue[i].easeType]?queue[i].easeType:easeType,
                     this
                 ),
                 true
-                ) 
+            )
                 :
                 undefined;
         }
     }
-    executor(context){
+    executor(context) {
         // super();
-        let {el,i,queue, duration, easeType,animationQueueHandler,next,manualNext}=context;
-        let perviousStatus=queue[i].props,
-            finalStatus=queue[i+1]?queue[i+1].props:undefined;
-        let delay=(queue[i+1] && queue[i+1].delay)?queue[i+1].delay:undefined;
-        if(!finalStatus){
+        let { el, i, queue, duration, easeType, animationQueueHandler, next, manualNext,status } = context;
+        if (!queue[i]) {
+            return
+        }
+        let perviousStatus = queue[i].props,
+            finalStatus = queue[i + 1] ? queue[i + 1].props : undefined;
+        let delay = (queue[i + 1] && queue[i + 1].delay) ? queue[i + 1].delay : undefined;
+        if (!finalStatus) {
             return
         }
 
-        easeType=queue[i+1].easeType?queue[i+1].easeType:easeType;
-        duration=queue[i+1].duration?queue[i+1].duration:duration;
+        easeType = queue[i + 1].easeType ? queue[i + 1].easeType : easeType;
+        duration = queue[i + 1].duration ? queue[i + 1].duration : duration;
 
-        let startTime = new Date().getTime();
+        status.startTime = new Date().getTime();
 
-        let totalDelta={};
+        let totalDelta = {};
 
-        for(let key in finalStatus){
-            totalDelta[key]=finalStatus[key]-parseInt(el.style[key])
+        for (let key in finalStatus) {
+            totalDelta[key] = finalStatus[key] - parseInt(el.style[key])
         }
-    
+
         let loop = () => {
-            let endTime = startTime + duration;
-            let currentTime = new Date().getTime();
-            let currentProgress = clamp((currentTime - startTime) / duration, 0, 1);
-            // console.log(el.style.width)
 
-            for(let key in perviousStatus){
-                el.style[key] = perviousStatus[key] + totalDelta[key] * ease[easeType](currentProgress) + 'px';
-            }
-    
-            if (currentProgress == 1) {
-                // clearInterval(timer)
-                cancelAnimationFrame(loop);
-                //如何执行下一步？
+            if (!context.status.paused) {
+                let endTime = status.startTime + duration;
+                let currentTime = new Date().getTime();
+                let currentProgress = clamp((currentTime - status.startTime) / duration, 0, 1);
 
-                setTimeout(()=>{
-                    if(queue[i+1].callback instanceof Function){
-                        queue[i+1].callback()
-                    }
-                    if(!manualNext){
-                        debugger
-                        next.call(context);
-                    }
-                },delay)
-                // debugger
-                return
+                // console.log(el.style.width)
+
+                for (let key in perviousStatus) {
+                    el.style[key] = perviousStatus[key] + totalDelta[key] * ease[easeType](currentProgress) + 'px';
+                }
+
+                if (currentProgress == 1) {
+                    // clearInterval(timer)
+                    cancelAnimationFrame(loop);
+                    //如何执行下一步？
+
+                    setTimeout(() => {
+                        if (queue[i + 1].callback instanceof Function) {
+                            queue[i + 1].callback()
+                        }
+                        if (!manualNext) {
+                            next.call(context);
+                        }
+                    }, delay)
+                    // debugger
+                    return
+                }
             }
+
             requestAnimationFrame(loop)
+
         }
         loop()
     }
 
-    next(){
-        this.i++;
-        this.animationQueueHandler.next();
+    next() {
+        if (!this.animationQueueHandler) {
+            this.animationQueueHandler = this.go();
+            setTimeout(() => this.animationQueueHandler.next(), this.queue[0].delay)
+        } else {
+            this.i++;
+            this.animationQueueHandler.next();
+        }
     }
 }
 
-Object.assign(Animation,{
+Object.assign(Animation, {
     getStyle
 })
 
