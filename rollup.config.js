50076e2e6c1c3c9dafe0efb57526f5d76f57e7a7
gogoend <gogoend@qq.com>
2020-03-18 21:59:00 +0800

    enhance: remove some useless dependency

--- rollup.config.js
+++ rollup.config.js
@@ -4,18 +4,18 @@ export default {
 	input: 'src/anikyu.js',
 	output: [
 		{
-			file: 'dist/anikyu.umd.js',
+			file: 'dist/anikyu.js',
 			name: 'Anikyu',
 			format: 'umd'
 		},
-		{
-			file: 'dist/anikyu.amd.js',
-			format: 'amd'
-		},
-		{
-			file: 'dist/anikyu.cjs.js',
-			format: 'cjs'
-		},
+		// {
+		// 	file: 'dist/anikyu.amd.js',
+		// 	format: 'amd'
+		// },
+		// {
+		// 	file: 'dist/anikyu.cjs.js',
+		// 	format: 'cjs'
+		// },
 		{
 			file: 'dist/anikyu.esm.js',
 			format: 'es'
