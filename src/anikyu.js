523420e8a96b6778431674253083878eab0964cd
gogoend <gogoend@qq.com>
2020-03-18 22:23:42 +0800

    enhance: import outter json and function bind() polyfill in demo

--- src/anikyu.js
+++ src/anikyu.js
@@ -1,7 +1,6 @@
 import Anikyu from './anikyu_class.js';
 
 import './polyfill/requestAnimationFrame.js';
-import './polyfill/functionBind.js';
 
 // 判断文件是如何引入的，如果是通过模块引入则不在全局暴露Anikyu
 // 直接在Webpack配置中改为UMD
