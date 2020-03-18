56d05dfee9b05b539d5c33c694a16b3a5c990f59
gogoend <gogoend@qq.com>
2020-03-18 22:24:13 +0800

    update builds

--- dist/anikyu.esm.js
+++ dist/anikyu.esm.js
@@ -534,30 +534,4 @@ for(var key$1 in anikyuStaticFunc){
 	};
 }(typeof window === 'undefined' ? global : window));
 
-(function (){
-	if (!Function.prototype.bind) {
-		Function.prototype.bind = function (oThis) {
-			if (typeof this !== 'function') {
-			// closest thing possible to the ECMAScript 5 internal IsCallable function
-				throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-			}
-  
-			var aArgs = Array.prototype.slice.call(arguments, 1), 
-				fToBind = this, 
-				fNOP = function () {},
-				fBound = function () {
-					return fToBind.apply(this instanceof fNOP && oThis
-						? this
-						: oThis || window,
-					aArgs.concat(Array.prototype.slice.call(arguments)));
-				};
-  
-			fNOP.prototype = this.prototype;
-			fBound.prototype = new fNOP();
-  
-			return fBound;
-		};
-	}
-})();
-
 export default Anikyu;
