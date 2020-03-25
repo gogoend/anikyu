5bc86cee4f37207788dc3dc275b7305329859561
gogoend <gogoend@qq.com>
2020-03-26 01:39:54 +0800

    enhance: move executor outside the class

--- src/anikyu_class.js
+++ src/anikyu_class.js
@@ -14,7 +14,7 @@ class Anikyu extends EventDoer {
 			easeType: 'quadraticInOut',
 			manualNext: false
 		};
-		this.config = Object.assign(JSON.parse(JSON.stringify(defaultConfig)),config);
+		this.config = Object.assign(JSON.parse(JSON.stringify(defaultConfig)), config);
 
 		this.i = 0;
 
@@ -26,137 +26,13 @@ class Anikyu extends EventDoer {
 			return;
 		}
 		if (!this.config.manualNext) {
-			this.executor();
+			executor.call(this);
 		}
 
-		this.executor = this.executor.bind(this);
+		// this.executor = this.executor.bind(this);
 
 		this.reqAniHandler = null;
 
-	}
-
-	// 动画执行器，用于在前后一对补间动画阶段之间进行补间
-	executor (index) {
-
-		if(!isNaN(parseInt(index))) {
-			this.i = index;
-		}
-
-		let { el, i, queue, next, status, config,reqAniHandler } = this;
-
-		cancelAnimationFrame(reqAniHandler);
-
-		if (!queue[i] || !queue[i + 1]) {
-			return;
-		}
-		let perviousStatus = queue[i].props,
-			finalStatus = queue[i + 1].props;
-
-		let delay = queue[i + 1].delay !== undefined ? queue[i + 1].delay : 0;
-		let currentStageIndex = this.i + 1;
-
-		// 确保每一次的初始状态都和前一对象中的属性相等
-		// 修复重播当前、跳转到、上一个、下一个函数不正常工作的问题
-		for (let key in perviousStatus) {
-			el[key] = perviousStatus[key];
-		}
-
-		let easeType = queue[i + 1].easeType ? queue[i + 1].easeType : config.easeType;
-		let duration = queue[i + 1].duration ? queue[i + 1].duration : config.duration;
-
-		let step = queue[i + 1].step ? queue[i + 1].step : undefined;
-
-		status.startTime = now() + delay;
-
-		let totalDelta = {};
-
-		for (let key in finalStatus) {
-			if(perviousStatus[key] === undefined){
-				// 当前一个状态不存在时首先尝试向前搜索，直到第0个
-				for(var j = i;j >= 0;j--){
-					if(queue[j].props[key] !== undefined) {
-						perviousStatus[key] = queue[j].props[key];
-						continue;
-					}
-					// 若到第0个仍然找不到则直接访问原始对象中相关属性
-					if(j === 0 && queue[j].props[key] === undefined){
-						if(el[key] !== undefined && !isNaN(parseFloat(el[key]))){
-							perviousStatus[key] = parseFloat(el[key]);
-						}else{
-							// 若依然访问不到，则直接设置该值为0
-							perviousStatus[key] = 0;
-						}
-					}
-				}
-
-			}
-			totalDelta[key] = finalStatus[key] - parseFloat(perviousStatus[key]);
-
-			// console.table ? 
-			// 	console.table({'final':finalStatus[key],'pervious':perviousStatus[key],'delta':totalDelta[key]})
-			// 	:
-			// 	console.log({'final':finalStatus[key],'pervious':perviousStatus[key],'delta':totalDelta[key]})
-			// ;
-			
-		}
-
-		let loop = () => {
-
-			if (!status.paused) {
-				// let endTime = status.startTime + duration;
-				let currentTime = now();
-				let currentProgress = clamp((currentTime - status.startTime) / duration, 0, 1);
-
-				let newValue = {},stageDelta = {},frameDelta = {};
-				for (let key in perviousStatus) {
-					newValue[key] = perviousStatus[key] + totalDelta[key] * ease[easeType].call(this, currentProgress, step);
-
-					stageDelta[key] = (newValue[key] === undefined ? 0 : newValue[key]) - (perviousStatus[key] === undefined ? 0 : perviousStatus[key]);
-
-					frameDelta[key] = (newValue[key] === undefined ? 0 : newValue[key]) - (el[key] === undefined ? 0 : parseFloat(el[key]));
-				}
-
-				Object.assign(el,newValue);
-				trigger(this,'animate',el ,{
-					stageIndex:this.i,
-					name:queue[currentStageIndex].name ? queue[currentStageIndex].name : '',
-					progress:currentProgress,
-					// target:el,
-					value:newValue,
-					stageDelta,
-					frameDelta
-				});
-				// if (queue[i + 1].onAnimating instanceof Function) {
-				// 	queue[i + 1].onAnimating(this);
-				// }
-				if (currentProgress == 1) {
-					// clearInterval(timer)
-					// cancelAnimationFrame(this.reqAniHandler);
-					// 如何执行下一步？
-
-					setTimeout(()=>{
-						// if (queue[i + 1].onFinished instanceof Function) {
-						// 	queue[i + 1].onFinished(this);
-						// }
-						for (let key in finalStatus) {
-							el[key] = finalStatus[key];
-						}
-						trigger(this,'finish',el ,{
-							stageIndex:currentStageIndex,
-							name:queue[currentStageIndex].name ? queue[currentStageIndex].name : ''
-						});
-						if (!config.manualNext) {
-							next.call(this);
-						}
-					}, delay);
-					// debugger
-					return;
-				}
-			}
-			this.reqAniHandler = requestAnimationFrame(loop);
-		};
-		setTimeout(loop,delay);
-		// loop();
 	}
 
 	// 动画流程控制
@@ -164,7 +40,7 @@ class Anikyu extends EventDoer {
 	pause () {
 		let { status } = this;
 
-		if(status.paused) return;
+		if (status.paused) return;
 
 		let pausedTime = now();
 		status.passedTime = pausedTime - status.startTime;
@@ -173,71 +49,190 @@ class Anikyu extends EventDoer {
 	resume () {
 		let { status } = this;
 
-		if(!status.paused) return;
+		if (!status.paused) return;
 
-		let startTime =  now();
+		let startTime = now();
 		status.startTime = startTime - status.passedTime;
 		status.paused = false;
 	}
 
 	replay () {
-		let {status,queue,i,executor,resume} = this;
+		let { status, queue, i, resume } = this;
 
-		if(!queue[i]) return;
-		if(status.paused) (resume.bind(this))();
+		if (!queue[i]) return;
+		if (status.paused) (resume.bind(this))();
 
-		executor(i);
+		executor.call(this, i);
 	}
 
 	// 跳转到、上一个、下一个
-	jump (index,finishCallFlag) {
-		let {status,queue,executor,resume} = this;
+	jump (index, finishCallFlag) {
+		let { status, queue, resume } = this;
 
-		if(!queue[index]) return;
-		if(status.paused) (resume.bind(this))();
+		if (!queue[index]) return;
+		if (status.paused) (resume.bind(this))();
 
-		executor(finishCallFlag ? index - 2 : index - 1);
-		// executor(index - 2);
+		executor.call(this, finishCallFlag ? index - 2 : index - 1);
 
 	}
 	prev () {
-		let {status,queue,i,executor,resume} = this;
-		if(!queue[i - 1]) return;
+		let { status, queue, i, resume } = this;
+		if (!queue[i - 1]) return;
 
-		if(status.paused) (resume.bind(this))();
+		if (status.paused) (resume.bind(this))();
 
 		this.i--;
-		executor();
+		executor.call(this);
 	}
 	next () {
-		let {status,queue,i,executor,resume} = this;
-		if(!queue[i + 1]) return;
+		let { status, queue, i, resume } = this;
+		if (!queue[i + 1]) return;
 
-		if(status.paused) (resume.bind(this))();
+		if (status.paused) (resume.bind(this))();
 
 		this.i++;
-		executor();
+		executor.call(this);
 	}
 
 	// 废弃
 	dispose () {
-		let { queue, i, reqAniHandler,el } = this;
+		let { queue, i, reqAniHandler, el } = this;
 
 		let currentStageIndex = i + 1;
 		cancelAnimationFrame(reqAniHandler);
-		trigger(this,'dispose',el,{
+		trigger(this, 'dispose', el, {
 			stageIndex: i,
 			name: queue[currentStageIndex].name ? queue[currentStageIndex].name : ''
 		});
-		// for(let key in this){
-		// 	this[key] = undefined;
-		// 	delete this[key];
-		// }
 	}
 }
 
+// 动画执行器，用于在前后一对补间动画阶段之间进行补间
+function executor (index) {
+
+	if (!isNaN(parseInt(index))) {
+		this.i = index;
+	}
+
+	let { el, i, queue, next, status, config, reqAniHandler } = this;
+
+	cancelAnimationFrame(reqAniHandler);
+
+	if (!queue[i] || !queue[i + 1]) {
+		return;
+	}
+	let perviousStatus = queue[i].props,
+		finalStatus = queue[i + 1].props;
+
+	let delay = queue[i + 1].delay !== undefined ? queue[i + 1].delay : 0;
+	let currentStageIndex = this.i + 1;
+
+	// 确保每一次的初始状态都和前一对象中的属性相等
+	// 修复重播当前、跳转到、上一个、下一个函数不正常工作的问题
+	for (let key in perviousStatus) {
+		el[key] = perviousStatus[key];
+	}
+
+	let easeType = queue[i + 1].easeType ? queue[i + 1].easeType : config.easeType;
+	let duration = queue[i + 1].duration ? queue[i + 1].duration : config.duration;
+
+	let step = queue[i + 1].step ? queue[i + 1].step : undefined;
+
+	status.startTime = now() + delay;
+
+	let totalDelta = {};
+
+	for (let key in finalStatus) {
+		if (perviousStatus[key] === undefined) {
+			// 当前一个状态不存在时首先尝试向前搜索，直到第0个
+			for (var j = i; j >= 0; j--) {
+				if (queue[j].props[key] !== undefined) {
+					perviousStatus[key] = queue[j].props[key];
+					continue;
+				}
+				// 若到第0个仍然找不到则直接访问原始对象中相关属性
+				if (j === 0 && queue[j].props[key] === undefined) {
+					if (el[key] !== undefined && !isNaN(parseFloat(el[key]))) {
+						perviousStatus[key] = parseFloat(el[key]);
+					} else {
+						// 若依然访问不到，则直接设置该值为0
+						perviousStatus[key] = 0;
+					}
+				}
+			}
+
+		}
+		totalDelta[key] = finalStatus[key] - parseFloat(perviousStatus[key]);
+
+		// console.table ? 
+		// 	console.table({'final':finalStatus[key],'pervious':perviousStatus[key],'delta':totalDelta[key]})
+		// 	:
+		// 	console.log({'final':finalStatus[key],'pervious':perviousStatus[key],'delta':totalDelta[key]})
+		// ;
+
+	}
+
+	let loop = () => {
+
+		if (!status.paused) {
+			// let endTime = status.startTime + duration;
+			let currentTime = now();
+			let currentProgress = clamp((currentTime - status.startTime) / duration, 0, 1);
+
+			let newValue = {}, stageDelta = {}, frameDelta = {};
+			for (let key in perviousStatus) {
+				newValue[key] = perviousStatus[key] + totalDelta[key] * ease[easeType].call(this, currentProgress, step);
+
+				stageDelta[key] = (newValue[key] === undefined ? 0 : newValue[key]) - (perviousStatus[key] === undefined ? 0 : perviousStatus[key]);
+
+				frameDelta[key] = (newValue[key] === undefined ? 0 : newValue[key]) - (el[key] === undefined ? 0 : parseFloat(el[key]));
+			}
+
+			Object.assign(el, newValue);
+			trigger(this, 'animate', el, {
+				stageIndex: this.i,
+				name: queue[currentStageIndex].name ? queue[currentStageIndex].name : '',
+				progress: currentProgress,
+				// target:el,
+				value: newValue,
+				stageDelta,
+				frameDelta
+			});
+			// if (queue[i + 1].onAnimating instanceof Function) {
+			// 	queue[i + 1].onAnimating(this);
+			// }
+			if (currentProgress == 1) {
+				// clearInterval(timer)
+				// cancelAnimationFrame(this.reqAniHandler);
+				// 如何执行下一步？
+
+				setTimeout(() => {
+					// if (queue[i + 1].onFinished instanceof Function) {
+					// 	queue[i + 1].onFinished(this);
+					// }
+					for (let key in finalStatus) {
+						el[key] = finalStatus[key];
+					}
+					trigger(this, 'finish', el, {
+						stageIndex: currentStageIndex,
+						name: queue[currentStageIndex].name ? queue[currentStageIndex].name : ''
+					});
+					if (!config.manualNext) {
+						next.call(this);
+					}
+				}, delay);
+				// debugger
+				return;
+			}
+		}
+		this.reqAniHandler = requestAnimationFrame(loop);
+	};
+	setTimeout(loop, delay);
+	// loop();
+}
+
 Object.assign(Anikyu, {
-	getStyle,rand,clamp
+	getStyle, rand, clamp
 });
 
 export default Anikyu;
