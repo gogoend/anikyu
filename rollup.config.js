456ad26e1fce22d46cee26e387b22534dbe88c4c
gogoend <gogoend@qq.com>
2020-07-19 18:44:51 +0800

    enhance: adjust the dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: './src/anikyu.js',
@@ -17,6 +18,9 @@ export default {
       extensions: ['.js'],
       ignoreGlobal: false,
       sourceMap: false,
+    }),
+    resolve({
+      browser:true
     }),
     babel({
       exclude: 'node_modules/**'
